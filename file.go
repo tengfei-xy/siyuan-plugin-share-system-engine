6c0c7310dab0b36c12b8b6871792259c5d3e2386
浔阳陌客 <virtualtengfei@163.com>
2024-04-05 14:00:40 +0800

    修改Savor的主题的图标失效问题

--- file.go
+++ file.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"archive/zip"
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -44,27 +45,39 @@ func unzip(dest, zipFile string) (err error) {
 	return nil
 }
 
-func check_theme_file(f string) error {
-	filename := filepath.Join(f, "appearance/themes/Odyssey/theme.css")
-	if tools.FileExist(filename) {
-		log.Info("修改主题 Odyssey")
-		return update_theme_file(filename)
-	}
-	filename = filepath.Join(f, "appearance/themes/Savor/theme.css")
-	if tools.FileExist(filename) {
-		log.Info("修改主题 Savor")
-		return update_theme_file(filename)
-	}
+func check_theme_file(dir string) error {
+	update_theme_file(dir, "Odyssey")
+	update_theme_file(dir, "Savor")
+
 	return nil
 }
-func update_theme_file(filename string) error {
+func update_theme_file(dir string, theme string) error {
+	filename := filepath.Join(dir, fmt.Sprintf("appearance/themes/%s/theme.css", theme))
+	if !tools.FileExist(filename) {
+		return nil
+	}
+
+	log.Infof("修改主题 %s", theme)
+
 	content, err := os.ReadFile(filename)
 	if err != nil {
 		log.Error(err)
 		return nil
 	}
 	s := string(content)
-	s = strings.ReplaceAll(s, "/appearance/themes/Odyssey/", "")
+	s = strings.ReplaceAll(s, fmt.Sprintf("/appearance/themes/%s/", theme), "")
 	os.WriteFile(filename, []byte(s), 0644)
+
+	filename = filepath.Join(dir, fmt.Sprintf("appearance/themes/%s/style/custom/link-icon.css", theme))
+
+	content, err = os.ReadFile(filename)
+	if err != nil {
+		log.Error(err)
+		return nil
+	}
+	s = string(content)
+	s = strings.ReplaceAll(s, fmt.Sprintf("/appearance/themes/%s/", theme), "../../")
+	os.WriteFile(filename, []byte(s), 0644)
+
 	return nil
 }
